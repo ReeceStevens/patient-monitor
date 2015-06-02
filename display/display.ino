9e3c56a1d53aeb9f73a60b22355c3d9a7502d057
asrastogi <asrastogi@utexas.edu>
2015-06-01 19:04:39 -0500

    alarm page additions

--- display/display.ino
+++ display/display.ino
@@ -8,7 +8,9 @@
 #include "interface.h"
 #include "ecg.h"
 
+#define ILI9341_PINK        0xF81F
 #define ILI9341_GREENYELLOW 0xAFE5      /* 173, 255,  47 */
+#define ILI9341_LIGHTGREY   0xC618      /* 192, 192, 192 */
 #define CS_TOUCH 8
 #define CS_SCREEN  10
 #define DC 9
@@ -19,11 +21,19 @@
 #define TS_MINY 130
 #define TS_MAXX 3800
 #define TS_MAXY 4000
+#define DEFAULT_ECG_MAX 98
+#define DEFAULT_ECG_MIN 90
+#define DEFAULT_SP02_MAX 100
+#define DEFAULT_SP02_MIN 95
 
 #define BOXSIZE 60
 
 int currentMode = 0; // Change mode
 int timeout = 0;
+int biasECGMax = 0;
+int biasECGMin = 0;
+int biasSP02Max = 0;
+int biasSP02Min = 0;
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(CS_SCREEN, DC);
 Adafruit_STMPE610 ts = Adafruit_STMPE610(CS_TOUCH);
@@ -39,14 +49,25 @@ Button hr_button = Button(0,0,BOXSIZE,BOXSIZE,ILI9341_RED,true,&tft);
 Button sp_button = Button(0,BOXSIZE,BOXSIZE,BOXSIZE,ILI9341_GREEN,true,&tft);
 Button temp_button = Button(0,BOXSIZE*2,BOXSIZE,BOXSIZE,ILI9341_BLUE,true,&tft);
 Button alarm_button = Button(260,200,BOXSIZE,BOXSIZE,ILI9341_RED,true,&tft);
-Button confirm_button = Button(0,200,BOXSIZE,BOXSIZE,ILI9341_GREEN,true,&tft);
-Button default_button = Button(BOXSIZE,200,BOXSIZE,BOXSIZE,ILI9341_RED,true,&tft);
-Button mode_button = Button(BOXSIZE*2,200,BOXSIZE,BOXSIZE,ILI9341_RED,true,&tft);
-Button cancel_button = Button(BOXSIZE*3,200,BOXSIZE,BOXSIZE,ILI9341_RED,true,&tft);
+Button confirm_button = Button(0,200,BOXSIZE,BOXSIZE,ILI9341_LIGHTGREY,true,&tft);
+Button default_button = Button(BOXSIZE,200,BOXSIZE,BOXSIZE,ILI9341_LIGHTGREY,true,&tft);
+Button mode_button = Button(BOXSIZE*2,200,BOXSIZE,BOXSIZE,ILI9341_LIGHTGREY,true,&tft);
+Button cancel_button = Button(BOXSIZE*3,200,BOXSIZE,BOXSIZE,ILI9341_LIGHTGREY,true,&tft);
+
+// Create settings page buttons
+Button ECGPlus = Button(30,70,18,18,ILI9341_BLACK,true,&tft);
+Button ECGMinus = Button(100,70,18,18,ILI9341_BLACK,true,&tft);
+Button ECGPlus1 = Button(180,70,18,18,ILI9341_BLACK,true,&tft);
+Button ECGMinus1 = Button(250,70,18,18,ILI9341_BLACK,true,&tft);
+Button SP02Plus = Button(30,120,18,18,ILI9341_WHITE,true,&tft);
+Button SP02Minus = Button(100,120,18,18,ILI9341_WHITE,true,&tft);
+Button SP02Plus1 = Button(180,120,18,18,ILI9341_WHITE,true,&tft);
+Button SP02Minus1 = Button(250,120,18,18,ILI9341_WHITE,true,&tft);
 
 
 // Create ECG trace
 ECGReadout ecg = ECGReadout(10,50,tft.height() - BOXSIZE, 100, 15 , 0, &tft);
+TS_Point p;
 
 /*
  * draw_submenu() - draws color box submenu on left of screen
@@ -81,7 +102,7 @@ void createVLabel(int x_coord, int y_coord, char* label, int textsize, int color
     tft.setCursor(x_coord, y_coord);
     tft.printf("%c", *label);
     label += 1;
-    y_coord += 8;
+    y_coord += 8*textsize;
   }
 }
 
@@ -144,6 +165,7 @@ void ECG_setup(){
   tft.println("60");
 }
 
+
 /*
  * sp02_setup() - intialize sp02 and % saturation interfaces
  */
@@ -194,6 +216,83 @@ void cancel_button_setup(void){
   createHLabel(BOXSIZE*3 + 14, 217, cancelLabel, 1, ILI9341_BLACK);
 }
 
+void ECGSettings_setup(void){
+  /* MINIMUM VALUE */
+  if (DEFAULT_ECG_MIN + biasECGMin < 100){
+    tft.setCursor(63, 72);
+    tft.setTextSize(2);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.printf("%d", DEFAULT_ECG_MIN + biasECGMin);
+  }
+  if (DEFAULT_ECG_MIN + biasECGMin > 99){
+    tft.setCursor(56, 72);
+    tft.setTextSize(2);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.printf("%d", DEFAULT_ECG_MIN + biasECGMin);
+  }
+  createVLabel(0, 55, ecgLabel, 2, ILI9341_GREEN);
+  ECGPlus.draw();
+  ECGMinus.draw();
+  createHLabel(34, 72, "-", 2, ILI9341_WHITE);
+  createHLabel(104, 72, "+", 2, ILI9341_WHITE);
+  
+  /* MAXIMUM VALUE */
+  if (DEFAULT_ECG_MAX + biasECGMax < 100){
+    tft.setCursor(213, 72);
+    tft.setTextSize(2);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.printf("%d", DEFAULT_ECG_MAX + biasECGMax);
+  }
+  if (DEFAULT_ECG_MAX + biasECGMax > 99){
+    tft.setCursor(206, 72);
+    tft.setTextSize(2);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.printf("%d", DEFAULT_ECG_MAX + biasECGMax);
+  }
+  ECGPlus1.draw();
+  ECGMinus1.draw();
+  createHLabel(184, 72, "-", 2, ILI9341_WHITE);
+  createHLabel(254, 72, "+", 2, ILI9341_WHITE);
+}
+
+void SP02Settings_setup(void){
+  /* MINIMUM VALUE */
+  if (DEFAULT_ECG_MIN + biasECGMin < 100){
+    tft.setCursor(63, 72);
+    tft.setTextSize(2);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.printf("%d", DEFAULT_ECG_MIN + biasECGMin);
+  }
+  if (DEFAULT_ECG_MIN + biasECGMin > 99){
+    tft.setCursor(56, 72);
+    tft.setTextSize(2);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.printf("%d", DEFAULT_ECG_MIN + biasECGMin);
+  }
+  createVLabel(0, 55, ecgLabel, 2, ILI9341_GREEN);
+  ECGPlus.draw();
+  ECGMinus.draw();
+  createHLabel(34, 72, "-", 2, ILI9341_WHITE);
+  createHLabel(104, 72, "+", 2, ILI9341_WHITE);
+  
+  /* MAXIMUM VALUE */
+  if (DEFAULT_ECG_MAX + biasECGMax < 100){
+    tft.setCursor(213, 72);
+    tft.setTextSize(2);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.printf("%d", DEFAULT_ECG_MAX + biasECGMax);
+  }
+  if (DEFAULT_ECG_MAX + biasECGMax > 99){
+    tft.setCursor(206, 72);
+    tft.setTextSize(2);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.printf("%d", DEFAULT_ECG_MAX + biasECGMax);
+  }
+  ECGPlus1.draw();
+  ECGMinus1.draw();
+  createHLabel(184, 72, "-", 2, ILI9341_WHITE);
+  createHLabel(254, 72, "+", 2, ILI9341_WHITE);
+}
 /*
  * clearScreen(int color) - clear screen with specified color
  */
@@ -219,6 +318,7 @@ void SettingsScreenInit(void){
   tft.drawFastVLine(BOXSIZE, 200, 40, ILI9341_BLACK);
   tft.drawFastVLine(BOXSIZE*2, 200, 40, ILI9341_BLACK);
   tft.drawFastVLine(BOXSIZE*3, 200, 40, ILI9341_BLACK);
+  ECGSettings_setup();
 }
 
 /*
@@ -236,34 +336,41 @@ void TIMER1_OVF_vect() {
 
 IntervalTimer myTimer;
 
+void fixCoordinates(int* x, int* y){
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+TS_Point getFixedCoordinates(void){
+   TS_Point p = ts.getPoint();
+   // Scale from 0-4000 to tft.width() using calibration numbers
+   p.x = tft.height() - map(p.x, TS_MINX, TS_MAXX, 0, tft.height());
+   p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.width());
+   int temp = p.x;
+   p.x = p.y;
+   p.y = temp;
+   return p;
+}
+
 void setup(void){
   gui_setup();
   product_title();  
   ECG_setup();
   sp02_setup();
   alarm_button_setup();
-  temperature_setup();
-  settings_setup();
+  //temperature_setup();
+  //settings_setup();
   ecg.read();
   myTimer.begin(isr, 150);
   myTimer.priority(128);
 }
 
-void isr(void) {
-	//clearScreen(ILI9341_BLUE);
-    ecg.read();
-	return;
-}
-
 int display_count = 0;
 int hr_counter = 0;
-void loop(void) {
-  /*main screen*/
-  if (currentMode == 0){
-    MainScreenInit();
-    while (currentMode == 0){
-      if (display_count >= 10) {
-         ecg.display_signal();
+void dispECGWave(void){
+  if (display_count >= 10) {
+        ecg.display_signal();
         display_count = 0;
         if (hr_counter >= 10) {
           int hr = ecg.heart_rate();
@@ -274,38 +381,48 @@ void loop(void) {
 	  tft.setTextColor(ILI9341_GREENYELLOW);
   	  tft.setTextSize(2);
   	  tft.println(s_hr);
-      } else { hr_counter += 1; }
+        } 
+        else { hr_counter += 1; }
       }
       else {
         display_count += 1;
       }
-      // Retrieve the touch point
-      TS_Point p = ts.getPoint();
-      // Scale from 0-4000 to tft.width() using calibration numbers
-      p.x = tft.height() - map(p.x, TS_MINX, TS_MAXX, 0, tft.height());
-      p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.width());
-      int temp = p.y;
-      p.y = p.x;
-      p.x = temp;
-      if (alarm_button.isTapped(p.x,p.y)){
+}
+void isr(void) {
+  if (currentMode == 0 && confirm_button.isTapped(p.x,p.y)){
         currentMode = 1;
-      }
+  }
+  if (currentMode == 1 && alarm_button.isTapped(p.x,p.y)){
+        currentMode = 0;
+  }    
+}
+
+
+void loop(void) {
+  /*main screen*/
+  if (currentMode == 1){
+    MainScreenInit();
+    while (currentMode == 1){
+      dispECGWave();
+      // Retrieve the touch point
+      p = getFixedCoordinates();
+      //TS_Point p = ts.getPoint();
+      // Scale from 0-4000 to tft.width() using calibration numbers
+      //p.x = tft.height() - map(p.x, TS_MINX, TS_MAXX, 0, tft.height());
+      //p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.width());
+      //fixCoordinates(&(p.x), &(p.y));
     }
   }
   /*alarm screen*/
-  if (currentMode == 1){
+  if (currentMode == 0){
     SettingsScreenInit();
-    while (currentMode == 1){
-      TS_Point p = ts.getPoint();
+    while (currentMode == 0){
+      p = getFixedCoordinates();
+      //TS_Point p = ts.getPoint();
       // Scale from 0-4000 to tft.width() using calibration numbers
-      p.x = tft.height() - map(p.x, TS_MINX, TS_MAXX, 0, tft.height());
-      p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.width());
-      int temp = p.y;
-      p.y = p.x;
-      p.x = temp;
-      if (confirm_button.isTapped(p.x,p.y)){
-        currentMode = 0;
-      }
+      //p.x = tft.height() - map(p.x, TS_MINX, TS_MAXX, 0, tft.height());
+      //p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.width());
+      //fixCoordinates(&(p.x), &(p.y));
     }
   }  
 }
